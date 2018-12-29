44bace283aabd0fc40e302b856f2cbb0644c8cc1
Keisha Perry <hire@keishaperry.com>
2018-12-28 18:52:30 -0800

    create custom db table on plugin activation

--- includes/class-toh-json-activator.php
+++ includes/class-toh-json-activator.php
@@ -30,7 +30,28 @@ class Toh_Json_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table = $wpdb->prefix . "toh_bonuses";
+		if($wpdb->get_var( "show tables like '$table'" ) != $table){
+			$sql_create_table = "CREATE TABLE $table (
+			 `id` int(11) NOT NULL auto_increment,
+			 `created_at` timestamp NOT NULL,
+			 `published_at` timestamp,
+			 `created_by`  VARCHAR(255) NOT NULL,
+			 `published_by` int(11),
+			 `bonus_info` longtext NOT NULL, 
+			 `state` int(2) NOT NULL, 
+			 PRIMARY KEY (`id`), 
+			 INDEX `TIMESTAMP` (`created_at`)
+		   ) $charset_collate; ";
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql_create_table );
+		} else {
+			//table already exists [todo?]
+		}
 	}
+
+
 
 }
