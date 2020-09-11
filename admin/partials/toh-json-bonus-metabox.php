9d28e1adf09c6c1fc68a8118d1f84c8744feae1e
Keisha Perry <keisha@hey.com>
2020-09-11 18:10:48 -0500

    fix state value causing errant space char

--- admin/partials/toh-json-bonus-metabox.php
+++ admin/partials/toh-json-bonus-metabox.php
@@ -36,7 +36,7 @@
         </div>
         <div>
             <label>State</label>
-            <input class="uk-input" type="text" name="state" id="state" placeholder="State" value="<?php echo isset($meta["_toh_state"][0]) ? $meta["_toh_state"][0] : "" ; ?> " maxlength='2'>
+            <input class="uk-input" type="text" name="state" id="state" placeholder="State" value="<?php echo isset($meta["_toh_state"][0]) ? $meta["_toh_state"][0] : "" ; ?>" maxlength='2'>
             <small>Two letter postal code</small>
         </div>
         <div>
